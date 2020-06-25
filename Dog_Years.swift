d2b7fcdcdf82abd63f37a8d08bd9a53377ae54ff
Sarah Ali <sarah08641@gmail.com>
2020-06-25 14:15:50 -0400

    Aydan's Code

---
+++ Dog_Years.swift
@@ -0,0 +1,9 @@
+// this is how old my dog is
+var dogAge = 2
+// the first two years of a dog's life is 21 dog years
+var earlyYears = 21
+// for every years past the inital two year, you add 4 years to the dogs age
+var laterYears = (dogAge - 2) * 4
+// total years
+var humanYears = (earlyYears+laterYears)
+print("My name is Chonk! Ruff ruff, I am \(humanYears) years old in human years")
