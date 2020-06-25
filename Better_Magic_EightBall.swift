d7cfc6531adfcd3e5c6c498d85348cb90be0a53f
Sarah Ali <sarah08641@gmail.com>
2020-06-25 14:10:49 -0400

    new code

---
+++ Better_Magic_EightBall.swift
@@ -0,0 +1,63 @@
+// ***PUT YOUR NAME HERE***
+var playerName = "Person"
+// ***PUT YOUR QUESTION HERE***
+var playerQuestion = "Will this work?"
+
+
+
+var randomNumber = Int.random(in: 1...9)
+var eightBall:String
+
+switch randomNumber {
+  case 1:
+  //23
+  eightBall = "    Yes, definitely    "
+  case 2:
+  //23
+  eightBall = "  It is decidedly so!  "
+  case 3:
+  //23
+  eightBall = "    Without a doubt    "
+  case 4:
+  //23
+  eightBall = " Reply hazy, try again "
+  case 5:
+  //23
+  eightBall = "    Ask again later    "
+  case 6:
+  //23
+  eightBall = "Better not tell you now"
+  case 7:
+  //23
+  eightBall = "   My sources say no   "
+  case 8:
+  //23
+  eightBall = "  Outlook not so good  "
+  case 9:
+  //23
+  eightBall = "     Very doubtful     "
+  default:
+  //5
+  eightBall = "         Error         "
+}
+
+if playerName == "" {
+  print("Please Enter Name")
+}else if playerQuestion == "" {
+  print("Please Enter Question")
+}else{
+    print("\(playerName) asks: \(playerQuestion)")
+print("                                      /  \\ ")
+print("                                    /      \\")
+print("                                  /          \\")
+print("                                /              \\")
+print("                              /                  \\")
+print("                            /                      \\")
+print("                          /  ---------------------   \\")
+print("                        /   \(eightBall)    \\    ")
+print("                      /      ----------------------      \\")
+print("                    /                                      \\")
+print("                  /                                          \\")
+print("                /______________________________________________\\")
+}
+
